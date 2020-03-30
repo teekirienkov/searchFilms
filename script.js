98fd11871cb3ee5ccfc8767a41e49acc47c2f944
Timur681 <teekirienkov@gmail.com>
2020-03-30 23:39:15 +0300

    add api

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Получение элементов со страницы
 const searchForm = document.querySelector('#search-form'); // поиск через id
 
+const API_KEY = '9c464a059d368b1b6fa45ea91caad68b';
 
 function apiSearch(event) {
-    event.preventDefault();
-
-    console.log(event);
+  event.preventDefault();    // Отмена обновления страницы при submit
 
 
+  const searchText = document.querySelector('.form-control').value, // Сохраняет только текст из инпута
+        server = 'https://api.themoviedb.org/3/search/multi?api_key=9c464a059d368b1b6fa45ea91caad68b&language=ru&query=&page=1&include_adult=true';
+  
 }
 
+
+
+
+
+
+
+// Обработчик события отправки формы
 searchForm.addEventListener('submit', apiSearch);
 
-let user = {
-    name: "Джон",
-    age: 30,
-  
-    sayHi() {
-      // this - это "текущий объект"
-      alert(this.name);
-    }
-  
-  };
-  
-user.sayHi(); // Джон
+
+// Функция получения данных из API
+function requestApi(url) {
+
+}
