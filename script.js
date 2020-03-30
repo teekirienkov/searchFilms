ca4f360be30d36a9e2b2f6e8aa154c802bbdb162
Timur681 <teekirienkov@gmail.com>
2020-03-31 01:37:47 +0300

    add new element

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Получение элементов со страницы
-const searchForm = document.querySelector('#search-form'); // поиск через id
+const searchForm = document.querySelector('#search-form'), // поиск через id
+      movie = document.querySelector('#movies');
 
 const API_KEY = '9c464a059d368b1b6fa45ea91caad68b';
 
 function apiSearch(event) {
-    event.preventDefault();    // Отмена обновления страницы при submit
+  event.preventDefault();    // Отмена обновления страницы при submit
 
 
   const searchText = document.querySelector('.form-control').value, // Сохраняет только текст из инпута
-    server = `https://api.themoviedb.org/3/search/multi?api_key=9c464a059d368b1b6fa45ea91caad68b&language=ru&query=' + ${searchText}`;
+        server = `https://api.themoviedb.org/3/search/multi?api_key=9c464a059d368b1b6fa45ea91caad68b&language=ru&query=' + ${searchText}`;
       
       
     requestApi(server);
@@ -41,5 +42,6 @@ function requestApi(url) {
     const output = JSON.parse(request.responseText); // Приводим JSON в нормальный вид
 
     console.log(output);
+
   });
 }
