2f965f6198945e1d7fe3c28b90be957a03e52657
Timur681 <teekirienkov@gmail.com>
2020-04-09 19:17:44 +0300

    add fetch

--- script.js
+++ script.js
@@ -8,15 +8,27 @@ function apiSearch(event) {
   event.preventDefault();    // Отмена обновления страницы при submit
   const searchText = document.querySelector('.form-control').value, // Сохраняет только текст из инпута
         server = `https://api.themoviedb.org/3/search/multi?api_key=9c464a059d368b1b6fa45ea91caad68b&language=ru&query=' + ${searchText}`;
-  requestApi(server)
-    .then(function(result){
-        const output = JSON.parse(result); // Приводим JSON в нормальный вид
-        let inner = '';
+  
+  fetch(server)
+    .then(function(value){
+      return value.json();
+    })
+    .then(function(output){
+      let inner = '';
         output.results.forEach(function (item) { // Перебираем массив и получаем названия фильмов/сериалов 
           let nameItem = item.name || item.title; // Выводится только название! (49-50 строка)
           console.log(nameItem);
           inner = inner + '<div class="col-3">' + nameItem + '</div>';
         });
+        movie.innerHTML = inner;
+    })
+    .catch(function(reason){
+      console.log(`Ошибка: ${reason.status}`);
+    });
+  
+  requestApi(server)
+    .then(function(result){
+        const output = JSON.parse(result); // Приводим JSON в нормальный вид 
     })
     .catch(function(reason){
       console.log(`Ошибка: ${reason.status}`);
@@ -25,7 +37,6 @@ function apiSearch(event) {
 
 // Обработчик события отправки формы
 searchForm.addEventListener('submit', apiSearch);
-
 
 // Функция получения данных из API
 // В request.responseText - содержится описание и т.д.
