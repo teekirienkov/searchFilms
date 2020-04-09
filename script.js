ef8c7d771461277b4265405ae19487c12be2b48d
Timur681 <teekirienkov@gmail.com>
2020-04-09 18:46:10 +0300

    add promise

--- script.js
+++ script.js
@@ -2,49 +2,56 @@
 const searchForm = document.querySelector('#search-form'), // поиск через id
       movie = document.querySelector('#movies');
 
+
+
+
 const API_KEY = '9c464a059d368b1b6fa45ea91caad68b';
 
 function apiSearch(event) {
   event.preventDefault();    // Отмена обновления страницы при submit
-
   const searchText = document.querySelector('.form-control').value, // Сохраняет только текст из инпута
         server = `https://api.themoviedb.org/3/search/multi?api_key=9c464a059d368b1b6fa45ea91caad68b&language=ru&query=' + ${searchText}`;
-      
-      
-    requestApi(server);
+  requestApi(server)
+    .then(function(result){
+      const output = JSON.parse(result); // Приводим JSON в нормальный вид
+      let inner = '';
+      output.results.forEach(function (item) { // Перебираем массив и получаем названия фильмов/сериалов 
+        let nameItem = item.name || item.title; // Выводится только название! (49-50 строка)
+        console.log(nameItem);
+        inner = inner + '<div class="col-3">' + nameItem + '</div>';
+    });
+    })
+    .catch(function(error){
+      console.log(error);
+    })
 }
-
 
 // Обработчик события отправки формы
 searchForm.addEventListener('submit', apiSearch);
+
+
 
 // Функция получения данных из API
 // В request.responseText - содержится описание и т.д.
 function requestApi(url) {
 
-  const request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.send();
+  return new Promise (function (resolve, reject){
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
 
-  request.addEventListener('readystatechange', function() {
-    if (request.readyState !== 4) return; // изменил jshitrc
-    if (request.status !== 200) {
-        console.log('Произошла ошибка:' + request.status);
+    request.addEventListener('load', function() {
+      if (request.status !== 200) {
+        reject({status: request.status});
         return;
-    }
-    // Парсим JSON файл
-    const output = JSON.parse(request.responseText); // Приводим JSON в нормальный вид
-
-    let inner = '';
-
-    // Перебираем массив и получаем названия фильмов/сериалов
-    // Выводится только название! (49-50 строка)
-    output.results.forEach(function (item) {
-        let nameItem = item.name || item.title;
-        console.log(nameItem);
-        inner = inner + '<div class="col-3">' + nameItem + '</div>';
+      } else {
+        resolve(request.response); // выполнение, в скобках передается ответ
+      }
     });
 
-    movie.innerHTML = inner;
-  }); // request.addEventListener
-}
+    request.addEventListener('error', function() {
+      reject({status: request.status});
+    })
+
+    request.send();
+  });
+};
