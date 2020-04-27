de1a6d5a22ab7128842626de233bb0da3f59b302
Timur681 <teekirienkov@gmail.com>
2020-04-27 22:28:31 +0300

    add new big function, rendering rating films

--- script.js
+++ script.js
@@ -67,3 +67,43 @@ function addEventMedia() {
 function showFullInfo() {
   console.log(this);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const urlTrends = 'https://api.themoviedb.org/3/trending/all/week?api_key=9c464a059d368b1b6fa45ea91caad68b&language=ru'
+  fetch(urlTrends)
+    .then((value) => {
+      if (value.status !== 200) {
+        return Promise.reject();
+      }
+      return value.json(); // этот метод включен только в объектах request, response
+    })
+    .then((output) => {
+      let inner = '<h3 class="col-12 text-center text-info">Популярное за неделю</h3>';
+      if(output.results.length === 0) {
+        inner = '<h2 class="col-12 text-center text-info">Ничего не найдено</h2>';
+      }
+
+      output.results.forEach(function (item) { // Перебираем массив и получаем названия фильмов/сериалов 
+        let nameItem = item.name || item.title; // Выводится только название!
+        const noPoster = item.poster_path ? urlPoster + item.poster_path : './img/withouth-poster.png';
+        
+        let dataInfo = '';
+        if (item.media_type !== 'person') {
+          dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+        
+        }
+        inner += `
+          <div class="col-12 col-md-4 col-x1-3 item">
+          <img src="${noPoster}" class="img_poster" alt="${nameItem}" ${dataInfo}>
+          <h5>${nameItem}</h5>
+          </div>`;
+      });
+
+      movie.innerHTML = inner;
+
+      addEventMedia();
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+})
