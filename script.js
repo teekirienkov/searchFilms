ab6389f85d327b5c3142f5fbcaaca04453339b77
Timur681 <teekirienkov@gmail.com>
2020-04-27 22:05:01 +0300

    refac

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Получение элементов со страницы
 const searchForm = document.querySelector('#search-form'), // поиск через id
       movie = document.querySelector('#movies'),
-      urlPoster = 'https://image.tmdb.org/t/p/w342';
+      urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 const API_KEY = '9c464a059d368b1b6fa45ea91caad68b';
 
@@ -22,20 +22,34 @@ function apiSearch(event) {
     })
     .then((output) => {
       let inner = '';
+      if(output.results.length === 0) {
+        inner = '<h2 class="col-12 text-center text-info">Ничего не найдено</h2>';
+      }
 
       output.results.forEach(function (item) { // Перебираем массив и получаем названия фильмов/сериалов 
         let nameItem = item.name || item.title; // Выводится только название!
+        const noPoster = item.poster_path ? urlPoster + item.poster_path : './img/withouth-poster.png';
         
-        const noPoster = item.poster_path ? urlPoster + item.poster_path : 'img/withouth-poster.png';
-
+        let dataInfo = '';
+        if (item.media_type !== 'person') {
+          dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+        
+        }
         inner += `
           <div class="col-12 col-md-4 col-x1-3 item">
-          <img src="${noPoster}" class="img_poster" alt="${nameItem}">
+          <img src="${noPoster}" class="img_poster" alt="${nameItem}" ${dataInfo}>
           <h5>${nameItem}</h5>
           </div>`;
       });
 
       movie.innerHTML = inner;
+
+      // Показ информации по клику на картинку
+      const media = movie.querySelectorAll('.item');
+
+      media.forEach(function(elem){
+        elem.addEventListener('click', showFullInfo);
+      });
     })
     .catch((reason) => {
       console.log(`Ошибка: ${reason}`);
@@ -44,3 +58,8 @@ function apiSearch(event) {
 
 // Обработчик события отправки формы
 searchForm.addEventListener('submit', apiSearch);
+
+
+function showFullInfo() {
+  console.log(this);
+}
